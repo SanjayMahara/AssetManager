63aba8b4c9b413a5f04e45598117c500cfac4230
SanjayMahara <sanjaymahara@gmail.com>
2017-01-10 13:13:37 +0530

    Commit changes to source Control

---
+++ Tables/dbo.Asset.sql
@@ -0,0 +1,8 @@
+CREATE TABLE [dbo].[Asset]
+(
+[Asset] [int] NOT NULL,
+[AssetName] [varchar] (30) COLLATE Latin1_General_CI_AI NOT NULL,
+[AssetCode] [varchar] (50) COLLATE Latin1_General_CI_AI NOT NULL,
+[AssetLocationId] [int] NOT NULL
+) ON [PRIMARY]
+GO
