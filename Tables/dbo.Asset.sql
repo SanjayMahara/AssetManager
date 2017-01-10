af1f732fef3bee7bffd138b37ce9b2a9733040ec
SanjayMahara <sanjaymahara@gmail.com>
2017-01-10 16:24:12 +0530

    Push changes to Repository

--- Tables/dbo.Asset.sql
+++ Tables/dbo.Asset.sql
@@ -3,6 +3,7 @@ CREATE TABLE [dbo].[Asset]
 [Asset] [int] NOT NULL,
 [AssetName] [varchar] (30) COLLATE Latin1_General_CI_AI NOT NULL,
 [AssetCode] [varchar] (50) COLLATE Latin1_General_CI_AI NOT NULL,
-[AssetLocationId] [int] NOT NULL
+[AssetLocationId] [int] NOT NULL,
+[AssetCreationDate] [date] NULL
 ) ON [PRIMARY]
 GO
